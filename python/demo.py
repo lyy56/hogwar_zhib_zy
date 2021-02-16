91c6f6457e9d6e19d34c41a61d95f4eebac4dc3e
liyaya <1564088177@qq.com>
2021-02-16 22:10:54 +0800

    python直播一作业

---
+++ python/demo.py
@@ -0,0 +1,21 @@
+def fun():
+    name = 'tome'
+    print(f'有参数={name}')
+
+
+def fun_1():
+    print('无参数')
+
+
+def ret():
+    return 1
+
+
+def ret_1():
+    print("无返回值")
+
+
+fun()
+fun_1()
+ret()
+ret_1()
